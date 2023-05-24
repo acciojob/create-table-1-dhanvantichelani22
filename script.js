66288a423c067961f0aa00d4dc1980bf7452523e
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-05-24 08:49:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  var table = document.getElementById("sampleTable");
+  var emptyRow = table.insertRow(0);
+  var cell1 = emptyRow.insertCell(0);
+  var cell2 = emptyRow.insertCell(1);
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
